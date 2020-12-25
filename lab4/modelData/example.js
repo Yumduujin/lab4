b64f63ca06006cfdac10906b8683dee55ffd0eee
Yumduujin <71216526+Yumduujin@users.noreply.github.com>
2020-12-25 15:26:15 +0800

    Add files via upload

---
+++ lab4/modelData/example.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var cs142models;
+
+if (cs142models === undefined) {
+   cs142models = {};
+}
+
+cs142models.exampleModel = function() {
+   return {
+      name: 'Web propgramming 2020',
+       motto: `project 4`,
+   };
+};
+
